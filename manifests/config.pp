2f7026eb658e8cabe4eb50d60abc42dc51182c6e
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-03-16 14:12:11 +0100

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,7 @@
 #
 class metricbeat::config {
 
+  # Configure .yml file using https://github.com/cataphract/puppet-yaml_settings module
   yaml_settings { '/etc/metricbeat/metricbeat.yml':
     values => $metricbeat::config, 
   }
